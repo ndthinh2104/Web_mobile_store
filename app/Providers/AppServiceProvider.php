9c2619b9cf1890146d60783a393484c1012cf141
Kientt <mrkientran.98@gmail.com>
2019-05-12 10:25:38 +0700

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\ProductType;
+use App\Cart;
+use Session;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,9 +14,22 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function register()
+    public function boot()
     {
-        //
+            view()->composer('header',function($view){
+                $loai_sp = ProductType::all();
+                
+                $view->with('loai_sp',$loai_sp);
+            });
+
+        view()->composer(['header','page.dat_hang'],function($view){
+            if(Session('cart')){
+                $oldCart = Session::get('cart');
+                $cart = new Cart($oldCart);
+                $view->with(['cart'=>Session::get('cart'), 'product_cart'=>$cart->items,'totalPrice'=>$cart->totalPrice,'totalQty'=>$cart->totalQty]);
+            }
+        });
+
     }
 
     /**
@@ -21,7 +37,7 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function register()
     {
         //
     }
--- routes/web.php
+++ routes/web.php
@@ -10,9 +10,75 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('index');
+    return view('welcome');
 });
-Route::get('login','LoginController@getLogin');
-Route::post('login','LoginController@postLogin');
+
+Route::get('index',[
+	'as'=>'trang-chu',
+	'uses'=>'PageController@getIndex'
+]);
+
+Route::get('loai-san-pham/{type}',[
+	'as'=>'loaisanpham',
+	'uses'=>'PageController@getLoaiSp'
+]);
+
+Route::get('chi-tiet-san-pham/{id}',[
+	'as'=>'chitietsanpham',
+	'uses'=>'PageController@getChitiet'
+]);
+
+Route::get('lien-he',[
+	'as'=>'lienhe',
+	'uses'=>'PageController@getLienHe'
+]);
+
+Route::get('gioi-thieu',[
+	'as'=>'gioithieu',
+	'uses'=>'PageController@getGioiThieu'
+]);
+
+Route::get('add-to-cart/{id}',[
+	'as'=>'themgiohang',
+	'uses'=>'PageController@getAddtoCart'
+]);
+
+Route::get('del-cart/{id}',[
+	'as'=>'xoagiohang',
+	'uses'=>'PageController@getDelItemCart'
+]);
+Route::get('dat-hang',[
+	'as'=>'dathang',
+	'uses'=>'PageController@getCheckout'
+]);
+
+Route::post('dat-hang',[
+	'as'=>'dathang',
+	'uses'=>'PageController@postCheckout'
+]);
+
+Route::get('dang-nhap',[
+	'as'=>'login',
+	'uses'=>'PageController@getLogin'
+]);
+Route::post('dang-nhap',[
+	'as'=>'login',
+	'uses'=>'PageController@postLogin'
+]);
+
+Route::get('dang-ki',[
+	'as'=>'signin',
+	'uses'=>'PageController@getSignin'
+]);
+
+Route::post('dang-ki',[
+	'as'=>'signin',
+	'uses'=>'PageController@postSignin'
+]);
+
+Route::get('dang-xuat',[
+	'as'=>'logout',
+	'uses'=>'PageController@postLogout'
+]);
+
